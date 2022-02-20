19360b5993052ed90df2ed6d20e9a6893c58980f
h_yashodha <h.yashodha@directfn.com>
2022-02-20 18:40:36 +0530

    init commit

---
+++ src/processes(x-axis)/01_05/finished/index.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+
+if (cluster.isMaster) {
+  console.log('this is the master process: ', process.pid)
+  for (let i=0; i<numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`worker process ${process.pid} had died`)
+    console.log(`starting new worker`)
+
+    /**
+     * Restart instance on exit
+     */
+    cluster.fork()
+  })
+
+} else {
+  console.log(`started a worker at ${process.pid}`)
+
+  http.createServer((req, res) => {
+    res.end(`process: ${process.pid}`)
+    if (req.url === '/kill') {
+      process.exit()
+    } else if (req.url === '/') {
+      console.log(`serving form ${process.pid}`)
+    }
+  }).listen(3000)
+}
